0be8d54233c28f9e8d7577f87597b050275fc6ba
KaterinaGrabovyk <katya.grabovik@ukr.net>
2024-04-16 22:34:04 +0300

    Version 1.0

--- KursovaConsole/DB/DataClasses.cs
+++ KursovaConsole/DB/DataClasses.cs
@@ -9,13 +9,13 @@ namespace KursovaConsole.DB
 {
     public class DataClasses
     {
-        public int ID { get; set; }
-        public string Virobnuk { get; set; }
-        public int RAM { get; set; }
-        public int HD { get; set; }
-        public int YadraCount { get; set; }
-        public double Price { get; set; }
-        public bool isInStock { get; set; }
+        public int ID { get; set; }// айді
+        public string Virobnuk { get; set; }//Виробник
+        public int RAM { get; set; }//RAM
+        public int HD { get; set; }//жорсткий диск
+        public int YadraCount { get; set; }//кількість ядер
+        public double Price { get; set; }//ціня
+        public bool isInStock { get; set; }//статус 
         public DataClasses(string virobnuk, int rAM, int hD, int yadraCount, double price, bool isInStock)
         {
             Virobnuk = virobnuk;
@@ -25,16 +25,16 @@ namespace KursovaConsole.DB
             Price = price;
             this.isInStock = isInStock;
         }
-        public virtual double Znijka(double zn) => -1;
-        public virtual string Podarok() => "";
+        public virtual double Znijka(double zn) => -1;//знижка
+        public virtual string Podarok() => "";//подарунок
 
-        public override string ToString() => $"ID:{ID}; Виробник:{Virobnuk};Об'єм RAM:{RAM};Об'єм жорского диску:{HD};Кількість ядер:{YadraCount};Ціна:[{Price}]грн;";
+        public override string ToString() => $"ID:{ID}; Виробник:{Virobnuk};Об'єм RAM:{RAM};Об'єм жорского диску:{HD};Кількість ядер:{YadraCount};Ціна:[{Price}]грн;";//для виводу інформацї
 
     }
     public class PC : DataClasses
     {
-        public string Model { get; set; }
-        public string Korpus { get; set; }
+        public string Model { get; set; }//модель
+        public string Korpus { get; set; }//корпус
         public PC(string virobnuk, int rAM, int hD, int yadraCount, double price, bool isInStock, string model, string korpus)
             : base(virobnuk, rAM, hD, yadraCount, price, isInStock)
         {
@@ -47,8 +47,8 @@ namespace KursovaConsole.DB
     }
     public class LapTop : DataClasses
     {
-        public double Diagonal { get; set; }
-        public string Dinamics { get; set; }
+        public double Diagonal { get; set; }//діагональ
+        public string Dinamics { get; set; }//динаміки
         public LapTop(string virobnuk, int rAM, int hD, int yadraCount, double price, bool isInStock, double diagonal, string dinamics)
             : base(virobnuk, rAM, hD, yadraCount, price, isInStock)
         {
